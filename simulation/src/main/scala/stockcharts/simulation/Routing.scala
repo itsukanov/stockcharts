e8d2b38e02bfbd20e416293f7d1d09636d2051ac
iatsukanov <iatsukanov@gmail.com>
2017-10-10 19:47:59 +0700

    Moved parsing simulation config inside  ws-command handler from web-socket initialization

--- simulation/src/main/scala/stockcharts/simulation/Routing.scala
+++ simulation/src/main/scala/stockcharts/simulation/Routing.scala
@@ -5,38 +5,52 @@ import java.util.UUID
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.scaladsl.{Flow, Source}
+import org.json4s.native.JsonMethods._
 import org.slf4j.LoggerFactory
 import stockcharts.Config.Stocks
 import stockcharts.models.SimulationConf
 
+import scala.util.{Failure, Success, Try}
+
 object Routing {
 
   val log = LoggerFactory.getLogger(this.getClass)
+  private implicit val formats = org.json4s.DefaultFormats
 
-  def wsCommandHandler(simulation: Source[String, _]) =
+  private def tryParseConf(confAsStr: String) = {
+    case class SimulationRequest(stock: String,
+                                 rsiBuy: Double,
+                                 rsiSell: Double,
+                                 takeProfit: Double,
+                                 stopLoss: Double)
+
+    Try {
+      val request = parse(confAsStr).extract[SimulationRequest]
+      val stock = Stocks.getById(request.stock).getOrElse(throw new RuntimeException("invalid stockId"))
+
+      SimulationConf(
+        simulationId = UUID.randomUUID().toString, stock, request.rsiBuy, request.rsiSell, request.takeProfit, request.stopLoss)
+    }
+  }
+
+  def wsCommandHandler(simulationFactory: SimulationConf => Source[String, _]) =
     Flow[Message].flatMapConcat {
-      case TextMessage.Strict("simulate") => // todo stop stream after simulation will be finished
-        simulation.map(TextMessage.apply)
+      case TextMessage.Strict(confAsStr) =>
+        tryParseConf(confAsStr) match {
+          case Success(conf) =>
+            val simulation = simulationFactory(conf).map(TextMessage.apply)
+            log.debug(s"Trade simulation for $conf has been created")
+            simulation
+
+          case Failure(thr) => Source.single(TextMessage(s"Can't extract simulation conf from '$confAsStr' because:\n${thr.getMessage}"))
+        }
 
       case _ => Source.single(TextMessage("Message unsupported"))
     }
 
   def apply(simulationFactory: SimulationConf => Source[String, _]) =
     path("simulate") {
-      parameters('stock,
-        'rsiBuy.as[Double],
-        'rsiSell.as[Double],
-        'takeProfit.as[Double],
-        'stopLoss.as[Double]) {
-        (stockId, rsiBuy, rsiSell, takeProfit, stopLoss) =>
-          val stock = Stocks.getById(stockId).getOrElse(throw new RuntimeException("invalid stockId")) // todo change to custom extractor
-          val simulationConf = SimulationConf(
-            simulationId = UUID.randomUUID().toString, stock, rsiBuy, rsiSell, takeProfit, stopLoss)
-          val tradeSimulation = simulationFactory(simulationConf)
-
-          log.debug(s"Trade simulation for $simulationConf has been created")
-          handleWebSocketMessages(wsCommandHandler(tradeSimulation))
-      }
+      handleWebSocketMessages(wsCommandHandler(simulationFactory))
     }
 
 }
