2948b0267bc959dbb079c9135fd32b24ee0616f1
iatsukanov <iatsukanov@gmail.com>
2017-10-26 18:45:37 +0700

    Reimplemented SimulationFactory via Source.fromGraph

--- simulation/src/main/scala/stockcharts/simulation/SimulationFactory.scala
+++ simulation/src/main/scala/stockcharts/simulation/SimulationFactory.scala
@@ -1,50 +1,39 @@
 package stockcharts.simulation
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.Source
+import akka.stream.SourceShape
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Source}
 import akka.util.Timeout
 import stockcharts.json.JsonConverting
 import stockcharts.kafka.{KafkaSource, OffsetReset}
 import stockcharts.models.{Money, Price, SimulationConf}
 import stockcharts.simulation.enrichers.indicators.RSIIndicator
-import stockcharts.simulation.enrichers.tradeevents.{AccountManager, TickIn}
-import stockcharts.simulation.enrichers.tradesignals.OverBoughtSoldStrategy
 import stockcharts.simulation.uisupport.UIModel
 
 object SimulationFactory {
-  import stockcharts.simulation.enrichers.NumericStreamsSupport._
   import stockcharts.simulation.enrichers.PriceStreamsSupport._
-  import stockcharts.simulation.enrichers.tradesignals.SimulationSupport._
   import stockcharts.simulation.uisupport.UIConverters._
 
   def simulateTrade(rsiPeriod: Int, initialBalance: Money, lotSize: Int)
                    (conf: SimulationConf)
-                   (implicit system: ActorSystem, to: Timeout): Source[UIModel, _] = {
-    val plainRows = KafkaSource(topic = conf.stock.topic, groupId = conf.simulationId, OffsetReset.Earliest)
-          .map(_.record.value())
+                   (implicit system: ActorSystem, to: Timeout): Source[UIModel, _] =
+    Source.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
 
-    val prices = plainRows.map(JsonConverting.toModel[Price])
-    val rsi = prices.calculate(RSIIndicator(rsiPeriod))
-    val tradeSignals = rsi
-      .map(_.value)
-      .calculateTradeSignals(OverBoughtSoldStrategy(conf.rsiSell, conf.rsiBuy))
+      val prices = KafkaSource(topic = conf.stock.topic, groupId = conf.simulationId, OffsetReset.Earliest)
+        .map(_.record.value())
+        .map(JsonConverting.toModel[Price])
 
-    val ticksIn = prices zip tradeSignals map { case (price, signal) => TickIn(price, signal) }
-    val accountChanges = calculateAccountChanges(ticksIn, AccountManager(
-      initialBalance,
-      constantSizeLotChooser(lotSize),
-      takeProfitStopLossChecker(conf.takeProfit, conf.stopLoss))
-    )
+      val toRSI = Flow[Price].map(identity).calculate(RSIIndicator(rsiPeriod)) // try to make `.calculate` as a separate Flow
 
-    val uiPrices = prices.map(x => toUIModel(x))
-    val uiIndicator = rsi.map(x => toUIModel(x))
+      val priceCache = b.add(Broadcast[Price](2))
+      val merge = b.add(Merge[UIModel](2))
 
-    val uiPrices2 = KafkaSource(topic = conf.stock.topic, groupId = conf.simulationId + "asd", OffsetReset.Earliest) // todo find how to use plainRows instead
-      .map(_.record.value())
-      .map(JsonConverting.toModel[Price])
-      .map(x => toUIModel(x))
+      prices ~> priceCache.in
 
-    uiPrices2 merge uiIndicator
-  }
+      priceCache.out(0).map(x => toUIModel(x)) ~> merge.in(0)
+      priceCache.out(1) ~> toRSI.map(x => toUIModel(x)) ~> merge.in(1)
+
+      SourceShape(merge.out)
+    })
 }
-
