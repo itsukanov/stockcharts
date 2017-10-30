80e57be9715f25776d235d4d14d873c057d36be3
iatsukanov <iatsukanov@gmail.com>
2017-10-30 15:02:24 +0700

    Simplified SimulationFactory.simulateTrade

--- simulation/src/main/scala/stockcharts/simulation/SimulationFactory.scala
+++ simulation/src/main/scala/stockcharts/simulation/SimulationFactory.scala
@@ -1,8 +1,8 @@
 package stockcharts.simulation
 
 import akka.actor.ActorSystem
-import akka.stream.FlowShape
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Zip}
+import akka.stream.{FlowShape, Outlet}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Source, Zip}
 import akka.util.Timeout
 import stockcharts.models.{Money, Price, SimulationConf}
 import stockcharts.simulation.enrichers.indicators.{IndicatorsSupport, RSIIndicator, RSIValue}
@@ -17,10 +17,25 @@ class SimulationFactory(rsiPeriod: Int,
                        (implicit system: ActorSystem, to: Timeout) {
   import stockcharts.simulation.enrichers.tradesignals.TradeSignalsSupport.calculateTradeSignals
   import stockcharts.simulation.uisupport.UIConverters._
+  import GraphDSL.Implicits._
 
-  def simulateTrade(conf: SimulationConf): Flow[Price, UIModel, _] =
+  def simulateTrade(conf: SimulationConf): Flow[Price, UIModel, _] = {
+    val accountManager = AccountManager(initialBalance,
+      lotSizeChooser = constantSizeLotChooser(lotSize),
+      isOrderReadyToClose = takeProfitStopLossChecker(conf.takeProfit, conf.stopLoss))
+
     Flow.fromGraph(GraphDSL.create() { implicit b =>
-      import GraphDSL.Implicits._
+      def ticksIn(prices: Outlet[Price], rsi: Outlet[RSIValue]) = {
+        val tradeSignals = rsi
+          .map(_.value)
+          .via(calculateTradeSignals(OverBoughtSoldStrategy(conf.overbought, conf.oversold)))
+
+        val priceAndTradeSignals = b.add(Zip[Price, Option[TradeSignal]]())
+        prices        ~> priceAndTradeSignals.in0
+        tradeSignals  ~> priceAndTradeSignals.in1
+        priceAndTradeSignals.out
+          .map { case (price, tradeSignal) => TickIn(price, tradeSignal) }
+      }
 
       val toRSI = IndicatorsSupport.calculating(RSIIndicator(rsiPeriod))
 
@@ -28,31 +43,19 @@ class SimulationFactory(rsiPeriod: Int,
       val uiModels = b.add(Merge[UIModel](4))
       val rsiCachedStage = b.add(Broadcast[RSIValue](2))
 
-      priceCachedStage.out(0)           ~> toUI[UIPrice]          ~> uiModels.in(0)  // uiPrices
-
-      priceCachedStage.out(1) ~> toRSI  ~> rsiCachedStage.in
-      rsiCachedStage.out(0)             ~> toUI[UIIndicatorValue] ~> uiModels.in(1)  // uiIndicatorValues
-
-      val priceAndTradeSignals = b.add(Zip[Price, Option[TradeSignal]]())
-      priceCachedStage.out(2) ~> priceAndTradeSignals.in0
-
-      val tradeSignals = rsiCachedStage.out(1)
-        .map(_.value)
-        .via(calculateTradeSignals(OverBoughtSoldStrategy(conf.overbought, conf.oversold)))
-      tradeSignals ~> priceAndTradeSignals.in1
-
-      val accountManager = AccountManager(initialBalance,
-        lotSizeChooser = constantSizeLotChooser(lotSize),
-        isOrderReadyToClose = takeProfitStopLossChecker(conf.takeProfit, conf.stopLoss))
+      priceCachedStage.outlet           ~> toUI[UIPrice]          ~> uiModels  // uiPrices
+      priceCachedStage.outlet ~> toRSI  ~> rsiCachedStage.in
+      rsiCachedStage.outlet             ~> toUI[UIIndicatorValue] ~> uiModels  // uiIndicatorValues
 
       val cachedTicksOut = b.add(Broadcast[TickOut](2))
-      priceAndTradeSignals.out
-        .map { case (price, tradeSignal) => TickIn(price, tradeSignal) }
+      ticksIn(priceCachedStage.outlet, rsiCachedStage.outlet)
         .via(calculateAccountChanges(accountManager)) ~> cachedTicksOut.in
 
-      cachedTicksOut.out(0) ~> toUI[UIAccount] ~> uiModels // uiAccounts
-      cachedTicksOut.out(1).mapConcat(_.events) ~> toUI[UITradeEvent] ~> uiModels // uiTradeEvents
+      cachedTicksOut.outlet                     ~> toUI[UIAccount]      ~> uiModels   // uiAccounts
+      cachedTicksOut.outlet.mapConcat(_.events) ~> toUI[UITradeEvent]   ~> uiModels   // uiTradeEvents
 
       FlowShape(priceCachedStage.in, uiModels.out)
     })
+  }
+
 }
